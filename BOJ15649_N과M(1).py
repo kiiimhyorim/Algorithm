56bfb2a3db5f561314acacedc8e7b9207e4137a9
kiiimhyorim <h_rim810@naver.com>
2022-02-06 23:12:03 +0900

    BOJ-15649-Backtracking-DFS

---
+++ BOJ15649_N과M(1).py
@@ -0,0 +1,15 @@
+n,m = map(int, input().split())
+stack = []
+
+def dfs():
+    if len(stack) == m:
+        print(*stack)
+        return
+    for i in range(1,n+1):
+        if i not in stack:
+            stack.append(i)
+            dfs()
+            
+            stack.pop()
+            
+dfs()
